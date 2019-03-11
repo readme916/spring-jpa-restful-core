a189ddfe251d97255dc32d3783811a51314126dc
readme916 <1290144599@qq.com>
2019-03-11 15:52:42 +0800

    fix

--- src/main/java/com/liyang/jpa/restful/core/config/SecurityEnabledToggle.java
+++ src/main/java/com/liyang/jpa/restful/core/config/SecurityEnabledToggle.java
@@ -15,14 +15,13 @@ import com.liyang.jpa.restful.core.controller.StructureShowController;
 @ConditionalOnProperty(value = "spring.jpa.restful.security.enabled", havingValue = "false")
 @ConditionalOnBean(value = StructureShowController.class)
 @Order(-1)
-public class SecurityDisabledConfig extends WebSecurityConfigurerAdapter {
+public class SecurityEnabledToggle extends WebSecurityConfigurerAdapter {
 
 	@Value("${spring.jpa.restful.structure-path}")
 	private String structurePath;
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable();
 		http.requestMatchers().antMatchers("/" + structurePath + "/**").and().authorizeRequests()
 				.antMatchers("/" + structurePath + "/**").permitAll();
 	}
