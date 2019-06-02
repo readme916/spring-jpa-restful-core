2c014be7e3911ce344d7ee137f59c053501d4327
readme916 <1290144599@qq.com>
2019-06-02 21:27:19 +0800

    1

--- src/main/java/com/liyang/jpa/restful/core/utils/CommonUtils.java
+++ src/main/java/com/liyang/jpa/restful/core/utils/CommonUtils.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
 import javax.validation.Validator;
@@ -15,6 +16,8 @@ import javax.validation.ValidatorFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.BeanWrapper;
 import org.springframework.beans.BeanWrapperImpl;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -29,7 +32,40 @@ import com.liyang.jpa.smart.query.exception.QueryException;
 
 public class CommonUtils {
 	
+	public static String getIPAddress() {
+		
+		HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
 	
+		String ip = null;
+ 
+		// X-Forwarded-For：Squid 服务代理
+		String ipAddresses = request.getHeader("X-Forwarded-For");
+		if (ipAddresses == null || ipAddresses.length() == 0 || "unknown".equalsIgnoreCase(ipAddresses)) {
+			// Proxy-Client-IP：apache 服务代理
+			ipAddresses = request.getHeader("Proxy-Client-IP");
+		}
+		if (ipAddresses == null || ipAddresses.length() == 0 || "unknown".equalsIgnoreCase(ipAddresses)) {
+			// WL-Proxy-Client-IP：weblogic 服务代理
+			ipAddresses = request.getHeader("WL-Proxy-Client-IP");
+		}
+		if (ipAddresses == null || ipAddresses.length() == 0 || "unknown".equalsIgnoreCase(ipAddresses)) {
+			// HTTP_CLIENT_IP：有些代理服务器
+			ipAddresses = request.getHeader("HTTP_CLIENT_IP");
+		}
+		if (ipAddresses == null || ipAddresses.length() == 0 || "unknown".equalsIgnoreCase(ipAddresses)) {
+			// X-Real-IP：nginx服务代理
+			ipAddresses = request.getHeader("X-Real-IP");
+		}
+		// 有些网络通过多层代理，那么获取到的ip就会有多个，一般都是通过逗号（,）分割开来，并且第一个ip为客户端的真实IP
+		if (ipAddresses != null && ipAddresses.length() != 0) {
+			ip = ipAddresses.split(",")[0];
+		}
+		// 还是不能获取到，最后再通过request.getRemoteAddr();获取
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ipAddresses)) {
+			ip = request.getRemoteAddr();
+		}
+		return ip;
+	}
 	public static Set<String> filterAutoFields(Set<String> fields){
 		HashSet<String> hashSet = new HashSet<String>();
 		for (String string : fields) {
